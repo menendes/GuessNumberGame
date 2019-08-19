4718287e0215277d5ad7421682c6db9cd595c40f
menendes <koyuncuibrahim1453@gmail.com>
2019-08-19 15:06:48 +0300

    Added Custom Fonts

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import {StyleSheet,View,Text} from 'react-native';
 
-const GameOverScreen = props => {
-  return (
-    <View style={styles.screen}>
-      <Text>The Game is Over!</Text>
-      <Text>Number of rounds: {props.roundsNumber}</Text>
-      <Text>Number was: {props.userNumber}</Text>
-      <Button title="NEW GAME" onPress={props.onRestart} />
-    </View>
-  );
+
+const GameOverScreen = props =>{
+    return (
+        <View style={styles.screen}>
+            <Text>Game is Over!</Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    screen:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
 });
 
 export default GameOverScreen;
