81b192b1920582af7060c9cd66ab3905eaf54c8f
menendes <koyuncuibrahim1453@gmail.com>
2019-08-19 23:00:15 +0300

    Added icons on the buttons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -4,6 +4,7 @@ import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
 import DefaultStyles from '../constants/default-styles';
+import MainButton from '../components/MainButton';
 
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
@@ -61,11 +62,8 @@ const GameScreen = props => {
       <Text style={DefaultStyles.title}>Opponent's Guess</Text>
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card style={styles.buttonContainer}>
-        <Button title="LOWER" onPress={nextGuessHandler.bind(this, 'lower')} />
-        <Button
-          title="GREATER"
-          onPress={nextGuessHandler.bind(this, 'greater')}
-        />
+        <MainButton  onPress={nextGuessHandler.bind(this, 'lower')} >Lower</MainButton>
+        <MainButton onPress={nextGuessHandler.bind(this, 'greater')}>Greater</MainButton>
       </Card>
     </View>
   );
@@ -81,8 +79,8 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-around',
     marginTop: 20,
-    width: 300,
-    maxWidth: '80%'
+    width: 400,
+    maxWidth: '90%'
   }
 });
 
